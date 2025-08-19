7bbf847aef40e3d2b30a5bce80759db70f355a45
Jakub Bryska <troppout@gmail.com>
2025-08-19 14:49:04 +0200

    delete unnecessary code, add maintenance service and simple request to test backend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaintenanceService } from './services/maintenance';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'frontend';
+  maintenanceService = inject(MaintenanceService);
+
+  constructor() {
+    effect(() => {
+      console.log(this.maintenanceService.maintenance());
+    });
+  }
 }
