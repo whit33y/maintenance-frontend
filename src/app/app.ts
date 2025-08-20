f881e4c94665b7bddb78f7a11a1a0a2518b449c3
Jakub Bryska <troppout@gmail.com>
2025-08-20 09:04:46 +0200

    feat: enhance CategoriesService with loadCategories and loadCategory methods

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaintenanceService } from './services/maintenance-service';
+import { CategoriesService } from './services/categories-service';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,11 @@ import { MaintenanceService } from './services/maintenance-service';
 export class App {
   protected title = 'frontend';
   maintenanceService = inject(MaintenanceService);
+  categoriesService = inject(CategoriesService);
 
   constructor() {
     this.maintenanceService.loadSingleMaintenance('bda2c6e6-79b8-43d8-8a82-22b3ec88d947');
+    this.categoriesService.loadCategory('22a739fb-92c0-484f-87d1-b5a5e255188e');
   }
 
   ngOnInit() {
--- src/app/services/categories-service.ts
+++ src/app/services/categories-service.ts
@@ -6,12 +6,48 @@ import { inject, Injectable, signal } from '@angular/core';
 })
 export class CategoriesService {
   http = inject(HttpClient);
-  private PATH = 'http://localhost:8000/api/categories';
   error = signal<string>('');
-  categories = signal<Categories[]>([]);
+  categories = signal<Category[]>([]);
+  selectedCategory = signal<Category | undefined>(undefined);
+
+  private PATH = 'http://localhost:8000/api/categories';
+
+  constructor() {
+    this.loadCategories();
+  }
+
+  loadCategories() {
+    this.http.get<Category[]>(this.PATH).subscribe({
+      next: data => {
+        this.categories.set(data);
+      },
+      error: err => {
+        console.error('Failed to load categories', err);
+        this.error.set(err.message);
+      },
+      complete: () => {
+        console.log('Categories loaded', this.categories());
+      },
+    });
+  }
+
+  loadCategory(id: string) {
+    this.http.get<Category>(`${this.PATH}/${id}`).subscribe({
+      next: data => {
+        this.selectedCategory.set(data);
+      },
+      error: err => {
+        console.error('Failed to load category', err);
+        this.error.set(err.message);
+      },
+      complete: () => {
+        console.log('Single category loaded', this.selectedCategory());
+      },
+    });
+  }
 }
 
-type Categories = {
+type Category = {
   id: string;
   name: string;
   is_private: boolean;
