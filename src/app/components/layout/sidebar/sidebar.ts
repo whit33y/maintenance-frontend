4947687874654abce850e0d9125e1a0f1628065b
whit33y <troppout@gmail.com>
2025-08-24 21:39:15 +0200

    feat: update Sidebar component to use routerLink

--- src/app/components/layout/sidebar/sidebar.ts
+++ src/app/components/layout/sidebar/sidebar.ts
@@ -1,12 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../services/auth-service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
   authService = inject(AuthService);
+  private router = inject(Router);
+
+  navigateTo(route: string) {
+    this.router.navigate([`/${route}`]);
+  }
 }
