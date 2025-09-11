c3559b8b25533fdd436cafc617b897703c2f7411
Jakub Bryska <troppout@gmail.com>
2025-09-11 14:21:51 +0200

    ./

--- src/app/components/elements/auth-form/auth-form.ts
+++ src/app/components/elements/auth-form/auth-form.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth-service';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth-service';
 
 @Component({
   selector: 'app-auth-form',
