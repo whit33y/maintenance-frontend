b4733964c97ed8d5a363d3c74fe92c41857947fd
Jakub Bryska <troppout@gmail.com>
2025-09-11 08:46:26 +0200

    fix: table to be more flexible

--- src/app/components/elements/table/table.ts
+++ src/app/components/elements/table/table.ts
@@ -1,5 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Category } from '../../../services/interfaces/categories.interface';
+import { Maintenance } from '../../../services/interfaces/maintenance.interface';
+
+function hasProperty<T extends object, K extends keyof T>(
+  obj: T,
+  prop: K,
+): obj is T & Record<K, string> {
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
 
 @Component({
   selector: 'app-table',
@@ -8,6 +16,13 @@ import { Category } from '../../../services/interfaces/categories.interface';
   styleUrl: './table.css',
 })
 export class Table {
-  @Input() columns: string[] = [];
-  @Input() data: Category[] = [];
+  @Input() columns: { header: string; field: string }[] = [];
+  @Input() data: Maintenance[] | Category[] = [];
+
+  getFieldValue(row: Category | Maintenance, field: string) {
+    if (hasProperty(row, field as keyof (Category | Maintenance))) {
+      return row[field as keyof (Category | Maintenance)];
+    }
+    return '';
+  }
 }
--- src/app/components/pages/maintenance/maintenance.ts
+++ src/app/components/pages/maintenance/maintenance.ts
@@ -3,10 +3,11 @@ import { MaintenanceService } from '../../../services/maintenance-service';
 import { MaintenanceEventsService } from '../../../services/maintenance-events-service';
 import { RemindersService } from '../../../services/reminders-service';
 import { Header } from '../../layout/header/header';
+import { Table } from '../../elements/table/table';
 
 @Component({
   selector: 'app-maintenance',
-  imports: [Header],
+  imports: [Header, Table],
   templateUrl: './maintenance.html',
   styleUrl: './maintenance.css',
 })
