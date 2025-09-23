4a1f548a5071de138d914ad8a974724ff3934b20
Jakub Bryska <troppout@gmail.com>
2025-09-23 13:13:49 +0200

    feat: add script to build project

---
+++ mynode.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({ path: './src/.env' });;
+
+console.log(process.env);
+
+const envFile = `export const environment = {
+    production: '${process.env.production}',
+    PATH: '${process.env.PATH}',
+};
+`;
+const targetPath = path.join(__dirname, './src/environments/environment.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+    }
+});
