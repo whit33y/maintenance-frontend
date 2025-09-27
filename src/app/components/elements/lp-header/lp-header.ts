92f0a0a3fe76adbeef479105595514fc0094016b
whit33y <troppout@gmail.com>
2025-09-27 18:46:46 +0200

    feat: added landing page and its components

---
+++ src/app/components/elements/lp-header/lp-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lp-header',
+  imports: [],
+  templateUrl: './lp-header.html',
+  styleUrl: './lp-header.css'
+})
+export class LpHeader {
+
+}
